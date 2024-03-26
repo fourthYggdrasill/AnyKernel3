068e374de2483d14c88c587ece5e92a5612fcfe4
Death8023 <93075648+Death8023@users.noreply.github.com>
2024-03-27 01:15:45 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=アリエス Kernel by Dimuuni
+kernel.string=Art_millet Kernel by Death
 do.devicecheck=1
 do.modules=0
 do.systemless=1
