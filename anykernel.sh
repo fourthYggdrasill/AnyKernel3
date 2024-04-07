34066128dd2ce5445e1be28807d87935519c2b9d
fourthYggdrasill <69509718+fourthYggdrasill@users.noreply.github.com>
2024-04-07 14:32:40 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Art_millet Kernel by Death
+kernel.string=Alioth.kernel for mine
 do.devicecheck=1
 do.modules=0
 do.systemless=1
